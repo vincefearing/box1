8477365b3974354bacac241a4b25c9626df71a86
Vincent <vincefearing@gmail.com>
2026-02-10 21:49:10 -0800

    Initial Commit

---
+++ box1/box1App.swift
@@ -0,0 +1,17 @@
+//
+//  box1App.swift
+//  box1
+//
+//  Created by Vincent Fearing on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct box1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
