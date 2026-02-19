61e45539e03af227cb643edb931691cf0519949e
Vincent <vincefearing@gmail.com>
2026-02-19 07:42:26 -0800

    Started working in data fetching and scraping

---
+++ scripts/constants.py
@@ -0,0 +1,23 @@
+GENERATION_BOUNDARIES = [
+    (1, 151),
+    (2, 251),
+    (3, 386),
+    (4, 493),
+    (5, 649),
+    (6, 721),
+    (7, 809),
+    (8, 905),
+    (9, 1025),
+]
+
+GENERATION_DEBUT_GAMES = {
+    1: ["Red", "Blue"],
+    2: ["Gold", "Silver"],
+    3: ["Ruby", "Sapphire"],
+    4: ["Diamond", "Pearl"],
+    5: ["Black", "White"],
+    6: ["X", "Y"],
+    7: ["Sun", "Moon"],
+    8: ["Sword", "Shield"],
+    9: ["Scarlet", "Violet"],
+}
